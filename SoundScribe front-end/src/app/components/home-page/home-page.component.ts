61b5209d900b40be6b8b5af1523ff3a2611cfd20
Paula <paulaoorjuela@gmail.com>
2024-05-23 19:00:30 -0500

    add publication-card component and publication-form component on templates

--- SoundScribe front-end/src/app/components/home-page/home-page.component.ts
+++ SoundScribe front-end/src/app/components/home-page/home-page.component.ts
@@ -1,13 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule} from '@angular/material/icon';
+import { PublicationCardComponent } from '../templates/publication-card/publication-card.component';
+import { MatButtonModule } from '@angular/material/button';
+import { CreatePublicationFormComponent } from '../templates/create-publication-form/create-publication-form.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PublicationCardComponent, MatIconModule, MatButtonModule, MatCardModule,],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+
+  publications = [1,1,1,1,1,1]
+
+  constructor(public modal: MatDialog) {
+
+  }
+
+  openCreatePublicationModal(){
+    this.modal.open(CreatePublicationFormComponent)
+  }
 
 }
--- SoundScribe front-end/src/app/components/templates/publication-card/publication-card.component.ts
+++ SoundScribe front-end/src/app/components/templates/publication-card/publication-card.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-publication-card',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './publication-card.component.html',
   styleUrl: './publication-card.component.css'
 })
